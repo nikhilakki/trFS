3b9faf6daee5cfe5c32862d4ac94c41b64955d79
Nikhil Akki <niikhilravindra@gmail.com>
2021-04-13 10:39:47 +0100

    license info and readme update

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-Tiny RESTful File Server
+## trFS a.k.a Tiny RESTful File Server
+
+* I made this in a couple of hours to access PDFs from RPI3 server to kindle (using the default browser which comes with it).
+This app is very basic and serves the purpose it was built for :)
+
+* Feel free to customize it as per your liking! 
+
+
+### How to install
+
+```bash
+git clone <repo path>
+cd <repo path>
+python3 -m pip install pipenv # if pipenv is not installed on your system
+python3 -m pipenv install # to install all python depedencies for this app
+```
+
+### How to run
+
+create .env file with following content (customize it as requried)
+```
+PORT=80
+SHAREPATH="${work_dir}/files"
+```
+then run the following commands on bash
+
+```bash
+export work_dir=$(pwd)
+./register.sh
+./status.sh
+```
+
+*Tested on Raspbian Linux for Rpi3*
+
+> License - GPLv3
+
+> Author - Nikhil Akki
+
+> Version - 0.1.0
--- serve.py
+++ serve.py
@@ -1,13 +1,33 @@
+# Copyright (C) 2021 Nikhil Akki
+# 
+# This file is part of trFS (Tiny RESTful File Server).
+# 
+# trFS (Tiny RESTful File Server) is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# trFS (Tiny RESTful File Server) is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with trFS (Tiny RESTful File Server).  If not, see <http://www.gnu.org/licenses/>.
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 from pathlib import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+folder_path = os.getenv("SHAREPATH", "/home/pi/share/files")
+port = os.getenv("PORT", 8080)
 
 app = FastAPI()
-
-folder_path = "/home/pi/share/files"
 app.mount("/share/", StaticFiles(directory=folder_path), name="files")
 
 @app.get("/")
@@ -17,7 +37,7 @@ def list_all():
     html_content = f"""
     <html>
         <head>
-            <title>Rpi3 Share File Server</title>
+            <title>Rpi Share File Server</title>
         </head>
         <body>
             <h1>Shared files -</h1>
@@ -29,5 +49,5 @@ def list_all():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
